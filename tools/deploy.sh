13e5680a35d0b44e9f388794be3ec890884a78d8
SriBalaji <iamthecloverly@gmail.com>
2022-08-25 23:15:19 +0530

    Misc changes

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -102,8 +102,8 @@ flush() {
 }
 
 deploy() {
-  git config --global user.name "GitHub Actions"
-  git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
+  git config --global user.name "SriBalaji"
+  git config --global user.email "iam.thecloverly@pm.me"
 
   git update-ref -d HEAD
   git add -A
